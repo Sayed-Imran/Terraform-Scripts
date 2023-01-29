e930c5ed9ddc504c4048e470b73ec50ce568b02b
PIIOT22NOV3006 <sayedimran00786@gmail.com>
2023-01-29 15:57:26 +0530

    Mentioning dependencies, changing variables and adding local-exec

--- gcp-remote-exec/main.tf
+++ gcp-remote-exec/main.tf
@@ -1,43 +1,51 @@
 resource "google_compute_firewall" "allow-http-ssh" {
-    name    = "allow-http-ssh"
-    network = "default"
-    allow {
-        protocol = "tcp"
-        ports    = ["80", "22"]
-    }
-    source_ranges = ["0.0.0.0/0"]
-    target_tags   = ["allow-http-ssh"]
+  name    = "allow-http-ssh"
+  network = "default"
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "22"]
+  }
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["allow-http-ssh"]
 }
 
 
 resource "google_compute_instance" "test-machine" {
-    name         = "test-machine"
-    machine_type = var.machine_type
-    zone         = var.zone
-    tags         = ["allow-http-ssh"]
-    boot_disk {
-        initialize_params {
-        image = var.image
-        }
-    }
-    network_interface {
-        network = "default"
-        access_config {
-        
-        }
-    }
+  name         = "test-machine"
+  machine_type = var.machine_type
+  zone         = var.zone
+  tags         = google_compute_firewall.allow-http-ssh.target_tags
 
-    provisioner "remote-exec" {
-        connection {
-            type        = "ssh"
-            user        = var.user
-            host        = google_compute_instance.test-machine.network_interface[0].access_config[0].nat_ip
-            private_key = file(var.privatekeypath)
-        }
-        inline = [
-            "sudo apt-get update",
-            "sudo apt-get install -y nginx"
-        ]
+  metadata = {
+    ssh-keys = "${var.user}:${file(var.publickeypath)}"
+  }
+  boot_disk {
+    initialize_params {
+      image = var.image
     }
-      
+  }
+  network_interface {
+    network = "default"
+    access_config {
+
+    }
+  }
+
+  provisioner "remote-exec" {
+    connection {
+      type        = "ssh"
+      user        = var.user
+      host        = google_compute_instance.test-machine.network_interface[0].access_config[0].nat_ip
+      private_key = file(var.privatekeypath)
+    }
+    inline = [
+      "sudo apt-get update",
+      "sudo apt-get install -y nginx"
+    ]
+  }
+
+  provisioner "local-exec" {
+    command = "chrome ${google_compute_instance.test-machine.network_interface[0].access_config[0].nat_ip}"
+  }
+
 }
--- gcp-remote-exec/variables.tf
+++ gcp-remote-exec/variables.tf
@@ -1,51 +1,51 @@
 variable "project" {
   type        = string
   description = "The project ID to deploy to"
-  default = "level-slate-373806"
+  default     = "level-slate-373806"
 }
 
 variable "region" {
   type        = string
   description = "The region to deploy to"
-  default = "us-central1"
-  
+  default     = "us-central1"
+
 }
 
 variable "zone" {
   type        = string
   description = "The zone to deploy to"
-  default = "us-central1-a"
+  default     = "us-central1-a"
 }
 
 variable "machine_type" {
   type        = string
   description = "The machine type to deploy to"
-  default = "e2-medium"
+  default     = "e2-medium"
 }
-  
+
 variable "image" {
   type        = string
   description = "The image to deploy to"
-  default = "ubuntu-os-cloud/ubuntu-2004-lts"
+  default     = "ubuntu-os-cloud/ubuntu-2004-lts"
 }
 
 
 
 variable "user" {
-    type = string
-    default = "sayedimran00786"
+  type    = string
+  default = "sayedimran00786"
 }
 
 variable "email" {
-    type = string
-    default = "sayedimran00786@gmail.com"
+  type    = string
+  default = "sayedimran00786@gmail.com"
 }
 variable "privatekeypath" {
-    type = string
-    default = "./id_rsa"
+  type    = string
+  default = "./gcp-key"
 }
 
 variable "publickeypath" {
-    type = string
-    default = "./id_rsa.pub"
+  type    = string
+  default = "./gcp-key.pub"
 }
